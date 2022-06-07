5799f3fc5c03f71b054fc92d1113228a0eccaa50
unknown <kekzhihui@gmail.com>
2022-06-07 22:52:56 +0800

    initial commit

---
+++ App.js
@@ -0,0 +1,45 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
+import names from './names';
+
+const namesAsObject = names.map((name) => {
+  return {
+    name: name
+  }
+});
+
+export default function App() {
+  
+  const renderItem = ({ item }) => {
+    return <View style={styles.listItem}>
+      <Text>{item.name}</Text>
+    </View>
+  }
+  
+  return (
+    <FlatList
+      data={namesAsObject}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.name}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  listItem: {
+    padding: 20,
+    margin: 10,
+    backgroundColor: "lightblue",
+    borderBottomColor: "#999",
+    borderBottomWidth: 1,
+    shadowColor: 'black',
+    shadowRadius: 5,
+    shadowOpacity: 0.2,
+    shadowOffset: { x: 0, height: 3 }
+  }
+});
